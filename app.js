ea489cacddbba3a29825d6a211553d8636276fc9
Anthony Lapham <lapham.2@wright.edu>
2018-10-28 10:36:41 -0400

    minor change

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-if (process.env.ENVIRONMENT !== 'production') {
-  require('dotenv').config()  
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
 }
 const express = require('express');
 const path = require('path');
